e3edcdd02b5719ae7abb022989cc84ad13d0714b
mercally1 <31991854+mercally1@users.noreply.github.com>
2024-08-19 17:57:56 -0600

    Building the DbContext

---
+++ Taximetro/Domain/Entities/Taxi.cs
@@ -0,0 +1,30 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.Entities;
+
+public class Taxi
+{
+    [Key]
+    public int TaxiId { get; set; }
+
+    [Required]
+    public int ConductorId { get; set; }
+
+    [Required, MaxLength(10)]
+    public string? Placa { get; set; }
+
+    [Required, MaxLength(20)]
+    public string? Marca { get; set; }
+
+    [Required, MaxLength(20)]
+    public string? Modelo { get; set; }
+
+    [Required, MaxLength(10)]
+    public int Ahno { get; set; }
+
+    [ForeignKey("ConductorId")]
+    public virtual Conductor? Conductor { get; set; }
+}
