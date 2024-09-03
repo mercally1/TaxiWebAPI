0f5f6ed453b14c2c86d932281afd4d501199b656
mercally1 <31991854+mercally1@users.noreply.github.com>
2024-09-03 12:28:01 -0600

    Feat building interface IConductorRepository

---
+++ Taximetro/Domain/Interfaces/Infrastructure/Repositories/IConductorRepository.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities;
+
+namespace Domain.Interfaces.Infrastructure.Repositories
+{
+    public interface IConductorRepository
+    {
+        Task<Conductor> GetAllConductorAsync(int ConductorId);
+
+        Task<Conductor> GetConductorAsync(int ConductorId);
+
+        Task<bool> AddConductorAsync(Conductor conductor);
+
+        Task<Conductor> ModifyConductorAsync(Conductor conductorId);
+
+        Task<bool> RemoveConductorAsync(int conductorId);
+    }
+}
