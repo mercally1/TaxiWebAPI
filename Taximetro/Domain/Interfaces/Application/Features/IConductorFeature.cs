9499c6611f860d4b8adde2cd30923f16d4438170
mercally1 <31991854+mercally1@users.noreply.github.com>
2024-09-03 16:08:06 -0600

    Feat building the interface IConducturFeature

---
+++ Taximetro/Domain/Interfaces/Application/Features/IConductorFeature.cs
@@ -0,0 +1,19 @@
+﻿using Application.Shared.DTOs.Conductor.Delete;
+using Application.Shared.DTOs.Conductor.Get;
+using Application.Shared.DTOs.Conductor.Post;
+using Application.Shared.DTOs.Conductor.Put;
+
+namespace Domain.Interfaces.Application.Features;
+
+public interface IConductorFeature
+{
+    Task<GetConductorAllResponse> GetAllConductorAsync(GetConductorAllRequest conductorResquest);
+
+    Task<GetConductorResponse> GetConductorAsync(GetConductorRequest getConductorRequest);
+
+    Task<PostConductorResponse> CreateConductorAsync(PostConductorRequest postConductorRequest);
+
+    Task<PutConductorResponse> ModifyConductorAsync(PutConductorRequest putConductorRequest);
+
+    Task<DeleteConductorResponse> DeleteConductorAsync(DeleteConductorRequest deleteConductorRequest);
+}
