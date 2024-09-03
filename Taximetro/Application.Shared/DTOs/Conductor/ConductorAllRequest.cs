2c6b723d2a2be2ba95b2268453ec088ad905d819
mercally1 <31991854+mercally1@users.noreply.github.com>
2024-09-03 13:40:56 -0600

    Feat building the DTOs ConductorAllResponse and Request

---
+++ Taximetro/Application.Shared/DTOs/Conductor/ConductorAllRequest.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.Shared.DTOs.Conductor;
+
+public class ConductorAllRequest
+{
+    [Required]
+    public int ConductorId { get; set; }
+
+    [Required]
+    public string? Nombre { get; set; }
+
+    [Required]
+    public string? Apellido { get; set; }
+
+    [Required]
+    public int Licencia { get; set; }
+}
---
+++ Taximetro/Application.Shared/DTOs/Conductor/ConductorAllResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace Application.Shared.DTOs.Conductor;
+
+public class ConductorAllResponse
+{
+    public int ConductorId { get; set; }
+
+    public string? Nombre { get; set; }
+
+    public string? Apellido { get; set; }
+
+    public int Licencia { get; set; }
+
+    public int Contacto { get; set; }
+}
