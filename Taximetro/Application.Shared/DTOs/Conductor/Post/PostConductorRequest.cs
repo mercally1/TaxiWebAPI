74565bb4866931ef7c32075462d30db139594a14
mercally1 <31991854+mercally1@users.noreply.github.com>
2024-09-03 15:30:30 -0600

    Fix including atributes DTO PostConductorRequest

--- Taximetro/Application.Shared/DTOs/Conductor/Post/PostConductorRequest.cs
+++ Taximetro/Application.Shared/DTOs/Conductor/Post/PostConductorRequest.cs
@@ -6,4 +6,16 @@ public class PostConductorRequest
 {
     [Required]
     public int ConductorId { get; set; }
+
+    [Required]
+    public string? Nombre { get; set; }
+
+    [Required]
+    public string? Apellido { get; set; }
+
+    [Required]
+    public int Licencia { get; set; }
+
+    [Required]
+    public int LicenciaId { get; set; }
 }
